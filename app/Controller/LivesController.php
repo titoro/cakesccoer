a95ddfc436f4d87c127c082951e039be5ee62f8b
titoro <vegi.m.8355@gmail.com>
2015-01-01 13:52:27 +0900

    add hashtag serach function

--- app/Controller/LivesController.php
+++ app/Controller/LivesController.php
@@ -67,24 +67,45 @@ class LivesController extends AppController{
        
        /* fopen使用 */
        $handle = fopen(APP.'/Text/team_hashtag.txt','r');
-       $hashtag_data = array();
+       $hashtag_temp = array();
+       $hashtag = array();
        
        $count = count(file(APP.'/Text/team_hashtag.txt'));
        //debug($count);
        
        for($i = 0; $i < $count; $i++){
-           $hashtag_data[] = fgets($handle);
+           $hashtag_temp[] = fgets($handle);
        }
-       fclose($handle);
-       debug($hashtag_data);
-       /*
-       while(fgets($handle)){
-           $hashtag[] = fgets($handle);
+       fclose($handle); //ファイルを閉じる
+       
+       //ファイルから取得した情報でハッシュタグを取得
+       for($i = 0; $i < count($hashtag_temp); $i++){
+           $temp = split(',', $hashtag_temp[$i]);
+           $hashtag[$temp[0]] = $temp;
+           //連想配列の中身からチーム名を除く（先頭)
+           //「キー」→「ハッシュタグ」の配列にする
+           unset($hashtag[$temp[0]][0]);
+           $hashtag[$temp[0]] = array_merge($hashtag[$temp[0]]);    //添え字を詰める
+           
        }
-       debug($hashtag);
-       //$hash_info = fgets($handle);
-       fclose($handle);
-       */
+       
+       $team_name = "セレッソ大阪";
+       $hashtag_result = array();   //ハッシュタグ結果保持
+       //ハッシュタグ検索処理
+       $h_result = $this->searchHashtag($tmhOAuth, $hashtag[$team_name], 8);
+       //ハッシュタグの取得結果から情報取り出し
+       //debug($h_result);
+       $h_result = $h_result->statuses;
+       
+       //検索結果から情報の取り出し
+      
+       foreach($h_result as $var){
+           $hashtag_result['text'][] = $var->text;
+           $hashtag_result['name'][] = $var->user->name;
+       }
+       debug($hashtag_result);
+       
+       
        /* Twitterのホームラインを取得*/
        $timeline = $this->Session->read('hometime_line');
        //debug($timeline);
@@ -98,6 +119,73 @@ class LivesController extends AppController{
        //debug($timeline['text']);
        //debug($timeline['name']);
     }
+    
+    /*Twitter 検索結果取得用メソッド
+     * $tmhOAuth tmhOAuthインスタンス
+     * $hashtag 検索キーワード
+     * $count 取得件数
+     *  */
+    public function searchHashtag($tmhOAuth, $hashtag, $count){
+        /**
+         * requestメソッドでTwitterAPIにリクエストを送る
+         * Twitterの検索結果を取得
+ 	 */
+        
+        $hashtag_1; //検索に使用するハッシュタグ
+        
+        if(count($hashtag) > 1){
+            //指定したハッシュタグが複数ある場合
+            //1つ目のハッシュタグを指定
+            $hashtag_temp = $hashtag[0];
+        }else if(count($hashtag) === 0 ){
+            //ハッシュタグを取得できなかった場合
+            return false;
+        }else{
+            //ハッシュタグを取得できた場合
+            $hashtag_1 = $hashtag[0];
+        }
+        
+        /*ハッシュタグ（ワード）に先頭#が含まれているか
+         * 先頭の#が含まれている場合、#を取り除く
+         */
+        /*  使用しないのでコメントアウト   */
+        //if(preg_match('/^#.*/', $hashtag_1)){
+        //    先頭の#を取り除く
+        //    $temp = preg_replace('/^\w.*/', $hashtag_1,$hashtag_1);
+        //    $hashtag_1 = str_replace('#', '', $hashtag_1);
+        //
+        //}
+        
+        //debug($hashtag_1);
+        
+        $status = $tmhOAuth->request(
+                "GET", // リクエストメソッド
+                 $tmhOAuth->url("1.1/search/tweets.json"), // エンドポイントを指定
+                 array('q' => urlencode($hashtag_1), 'count' => $count));
+
+        /**
+        * requestの返り値はHTTPのステータスコード
+        *
+        */
+
+        if($status == 200){
+              /**
+              * データはメンバのresponseの中に、
+              * さらに生のデータはその中の"response"の中にJSON形式で格納されている
+              */
+
+              $response = $tmhOAuth->response;
+              //debug($response['response']);
+              $data_j = json_decode($response['response']);
+              //debug($data_j);
+              return $data_j;
+        }
+        else{
+            echo "リクエストの値が取得できませんでした";
+        }
+
+    }
+    
 
     /*Twitter 検索結果取得用メソッド
      * $tmhOAuth tmhOAuthインスタンス
